72e1f4cf2768783b6f3dd8821f85acc2c9f43740
Jackson Aden <jacksonaden@gmail.com>
2025-02-22 17:01:31 -0700

    Have working image, and button that changes background using React States. Need to work on Navigation

--- app/Screens/Home.tsx
+++ app/Screens/Home.tsx
@@ -1,19 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  Image,
+} from "react-native";
 
 const Home = ({ navigation }) => {
+  const [bgColor, setBgColor] = useState("gray");
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
       <Text>Welcome to RavensRoost! Which Script are you playing?</Text>
       <ScrollView>
         <Button
-          onPress={() =>
-            navigation.navigate("NoGreaterJoy", { name: "No Greater Joy" })
-          }
+          onPress={() => navigation.navigate("NoGreaterJoy")}
           title="No Greater Joy"
           color={"blue"}
           accessibilityLabel="No Greater Joy navigation Button"
         />
+        <Button
+          onPress={() => setBgColor(bgColor === "black" ? "gray" : "black")}
+          title="I'm a button!"
+          color={"blue"}
+          accessibilityLabel="Button Changer"
+        />
+        <Image source={require("./botc.png")} />
       </ScrollView>
     </View>
   );
@@ -24,7 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "gray",
   },
 });
 
--- app/app.tsx
+++ app/app.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./Screens/Home";
 import NoGreaterJoy from "./Screens/NoGreaterJoy";
 
-const Stack = createNativeStackNavigator<{
-  Home: undefined;
-  NoGreaterJoy: undefined;
-}>();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+function MyStack() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="NoGreaterJoy" component={NoGreaterJoy} />
       </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
     </NavigationContainer>
   );
 }
